418c5ca9565edbf38f9c7d292bd6b9c78f5600a7
DayX2 <aliumutgungor@gmail.com>
2025-01-10 13:24:11 +0000

    Create empty classes

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello from the Library project')
+class Book:
+    pass
+
+class Library:
+    pass
+
+book1 = Book()
+book2 = Book()
+
+library1 = Library()
+library2 = Library()
+
+print(library1)
+print(library2)
+print(book1)
+print(book2)
