51a62415a4a5ee1e0463157f3dd63b42fbb1d9fe
DayX2 <aliumutgungor@gmail.com>
2025-01-16 11:43:14 +0000

    Create empty classes

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class User(Person):
+    def __init__(self, name, age, user_id):
+        super().__init__(name, age)
+        self.user_id = user_id
+
+class Librarian(Person):
+    def __init__(self, name, age, employee_id):
+        super().__init__(name, age)
+        self.employee_id = employee_id
+
 class Book:
-    pass
-
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
 class Library:
-    pass
+    def __init__(self):
+        self.books = []
 
-book1 = Book()
-book2 = Book()
+    def add_book(self, book):
+        self.books.append(book)
 
-library1 = Library()
-library2 = Library()
+    def list_books(self):
+        for book in self.books:
+            print(book)
 
-print(library1)
-print(library2)
-print(book1)
-print(book2)
+user = User("Alice", 20, 4201)
+print(user.name, user.age, user.user_id)
+
+librarian = Librarian("Bob", 40, 10749386)
+print(librarian.name, librarian.age, librarian.employee_id)
+
+book1 = Book("1984", "George Orwell")
+book2 = Book("Pride and Prejudice", "Jane Austen")
+
+library = Library()
+library.add_book(book1)
+library.add_book(book2)
+
+library.list_books()
